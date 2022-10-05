73554310848cb5b9ba5a7bfc9c102f9453c34b61
AnhQuoc8683 <anhquoc8683@gmail.com>
2022-10-05 16:34:44 +0700

    1

---
+++ TOOL CHANGE USER AGENT ANH QUOCV1/TOOL CHANGE USER AGENT ANH QUOCV1/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace TOOL_CHANGE_USER_AGENT_ANH_QUOCV1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.TabConfiguration = new System.Windows.Forms.TabPage();
+            this.Txtpasswork = new System.Windows.Forms.TextBox();
+            this.TxtUsedname = new System.Windows.Forms.TextBox();
+            this.Txttaikhoan = new System.Windows.Forms.TextBox();
+            this.Txt2fa = new System.Windows.Forms.TextBox();
+            this.TxtIP = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnLoginmail = new System.Windows.Forms.Button();
+            this.BtnOpenChrome = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.TabConfiguration.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.TabConfiguration);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(881, 522);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // TabConfiguration
+            // 
+            this.TabConfiguration.Controls.Add(this.Txtpasswork);
+            this.TabConfiguration.Controls.Add(this.TxtUsedname);
+            this.TabConfiguration.Controls.Add(this.Txttaikhoan);
+            this.TabConfiguration.Controls.Add(this.Txt2fa);
+            this.TabConfiguration.Controls.Add(this.TxtIP);
+            this.TabConfiguration.Controls.Add(this.label5);
+            this.TabConfiguration.Controls.Add(this.label7);
+            this.TabConfiguration.Controls.Add(this.label6);
+            this.TabConfiguration.Controls.Add(this.label4);
+            this.TabConfiguration.Controls.Add(this.label3);
+            this.TabConfiguration.Controls.Add(this.label2);
+            this.TabConfiguration.Controls.Add(this.BtnLoginmail);
+            this.TabConfiguration.Controls.Add(this.BtnOpenChrome);
+            this.TabConfiguration.Location = new System.Drawing.Point(4, 24);
+            this.TabConfiguration.Name = "TabConfiguration";
+            this.TabConfiguration.Padding = new System.Windows.Forms.Padding(3);
+            this.TabConfiguration.Size = new System.Drawing.Size(873, 494);
+            this.TabConfiguration.TabIndex = 0;
+            this.TabConfiguration.Text = "Run configuration";
+            this.TabConfiguration.UseVisualStyleBackColor = true;
+            this.TabConfiguration.Click += new System.EventHandler(this.TabConfiguration_Click);
+            // 
+            // Txtpasswork
+            // 
+            this.Txtpasswork.Location = new System.Drawing.Point(630, 382);
+            this.Txtpasswork.Name = "Txtpasswork";
+            this.Txtpasswork.Size = new System.Drawing.Size(164, 23);
+            this.Txtpasswork.TabIndex = 2;
+            this.Txtpasswork.Text = "0fshfusifsf";
+            // 
+            // TxtUsedname
+            // 
+            this.TxtUsedname.Location = new System.Drawing.Point(481, 382);
+            this.TxtUsedname.Name = "TxtUsedname";
+            this.TxtUsedname.Size = new System.Drawing.Size(143, 23);
+            this.TxtUsedname.TabIndex = 2;
+            this.TxtUsedname.Text = "ProxyUS000644";
+            // 
+            // Txttaikhoan
+            // 
+            this.Txttaikhoan.Location = new System.Drawing.Point(86, 223);
+            this.Txttaikhoan.Name = "Txttaikhoan";
+            this.Txttaikhoan.Size = new System.Drawing.Size(708, 23);
+            this.Txttaikhoan.TabIndex = 2;
+            // 
+            // Txt2fa
+            // 
+            this.Txt2fa.Location = new System.Drawing.Point(309, 270);
+            this.Txt2fa.Name = "Txt2fa";
+            this.Txt2fa.Size = new System.Drawing.Size(233, 23);
+            this.Txt2fa.TabIndex = 2;
+            this.Txt2fa.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // TxtIP
+            // 
+            this.TxtIP.Location = new System.Drawing.Point(86, 382);
+            this.TxtIP.Name = "TxtIP";
+            this.TxtIP.Size = new System.Drawing.Size(233, 23);
+            this.TxtIP.TabIndex = 2;
+            this.TxtIP.Text = "185.202.174.221:56789";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(660, 364);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 15);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "PassWork";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(346, 205);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(138, 15);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "ID|Pass|2Fa|Mail|Pasmail";
+            this.label7.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(410, 205);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 15);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "UsedName";
+            this.label6.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(528, 364);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "UsedName";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(175, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Port";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 364);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "IP :";
+            // 
+            // BtnLoginmail
+            // 
+            this.BtnLoginmail.BackColor = System.Drawing.Color.IndianRed;
+            this.BtnLoginmail.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnLoginmail.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.BtnLoginmail.Location = new System.Drawing.Point(125, 441);
+            this.BtnLoginmail.Name = "BtnLoginmail";
+            this.BtnLoginmail.Size = new System.Drawing.Size(150, 50);
+            this.BtnLoginmail.TabIndex = 0;
+            this.BtnLoginmail.Text = "Login Mail";
+            this.BtnLoginmail.UseVisualStyleBackColor = false;
+            this.BtnLoginmail.Click += new System.EventHandler(this.BtnLoginmail_Click);
+            // 
+            // BtnOpenChrome
+            // 
+            this.BtnOpenChrome.BackColor = System.Drawing.SystemColors.Highlight;
+            this.BtnOpenChrome.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnOpenChrome.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.BtnOpenChrome.Location = new System.Drawing.Point(583, 441);
+            this.BtnOpenChrome.Name = "BtnOpenChrome";
+            this.BtnOpenChrome.Size = new System.Drawing.Size(150, 50);
+            this.BtnOpenChrome.TabIndex = 0;
+            this.BtnOpenChrome.Text = "Login Facebook";
+            this.BtnOpenChrome.UseVisualStyleBackColor = false;
+            this.BtnOpenChrome.Click += new System.EventHandler(this.BtnOpenChrome_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(873, 494);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 537);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bản quyền thuộc về Anh Quốc SĐT: 0395786831";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TOOL CHANGE USER AGENT ANH QUOC V1";
+            this.tabControl1.ResumeLayout(false);
+            this.TabConfiguration.ResumeLayout(false);
+            this.TabConfiguration.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage TabConfiguration;
+        private TabPage tabPage2;
+        private Label label1;
+        private Button BtnOpenChrome;
+        private TextBox Txtpasswork;
+        private TextBox TxtUsedname;
+        private TextBox TxtIP;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox Txttaikhoan;
+        private Label label7;
+        private Label label6;
+        private Button BtnLoginmail;
+        private TextBox Txt2fa;
+    }
+}
---
+++ TOOL CHANGE USER AGENT ANH QUOCV1/TOOL CHANGE USER AGENT ANH QUOCV1/Form1.cs
@@ -0,0 +1,210 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Keys = System.Windows.Forms.Keys;
+
+
+namespace TOOL_CHANGE_USER_AGENT_ANH_QUOCV1
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        IWebDriver driver;
+        string ExtensionPart = "C:\\Users\\ANH QUOC PRO\\Desktop\\Selenium\\TOOL CHANGE USER AGENT ANH QUOCV1\\TOOL CHANGE USER AGENT ANH QUOCV1\\TOOL CHANGE USER AGENT ANH QUOCV1\\bin\\Debug\\Extension";
+        private void BtnOpenChrome_Click(object sender, EventArgs e)
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            //driver = new ChromeDriver(service);
+            ////try
+            ////{
+            ////    driver.Close();
+            ////    driver.Quit();
+            ////}
+            ////catch (Exception ex)
+            ////{
+            ////    MessageBox.Show(ex.Message);
+            ////}
+            //driver.Navigate().GoToUrl("https://mbasic.facebook.com/");
+            string IP = TxtIP.Text;
+            string UserName = TxtUsedname.Text;
+            string Password = Txtpasswork.Text;
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments(new string[] { "--disable-notifications", "--window-size="+400+","+600,"--no-sandbox","disable-gpu","--disable-dev-shm-usage"});
+            options.AddExcludedArgument("enable-automation");
+            options.AddArgument("--blink-settings=imagesEnabled=false");
+            if (!string.IsNullOrEmpty(IP))
+            {
+                if (!string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(Password))
+                {
+                    options.AddExtension(ExtensionPart + "\\Auto Proxy Auth.crx");
+                }
+                options.AddArgument(string.Format("--proxy-server={0}", IP));
+                
+            }
+            ChromeDriver driver = new ChromeDriver(service, options);
+            Thread.Sleep(3000);
+            //driver.Close();
+
+            if (!string.IsNullOrEmpty(IP))
+            {
+                if (!string.IsNullOrEmpty(UserName) && !string.IsNullOrEmpty(Password))
+                {
+                    ////driver.Navigate().Refresh();
+                    //driver.Navigate().GoToUrl("chrome-extension://ggmdpepbjljkkkdaklfihhngmmgmpggp/options.html");
+                    //IWebElement User = driver.FindElement(By.XPath("/html/body/div[2]/div[2]/div[1]/div[1]/input"));
+                    //User.SendKeys(UserName);
+                    //IWebElement Pass = driver.FindElement(By.Id("password"));
+                    //Pass.SendKeys(Password);
+                    //IWebElement retry = driver.FindElement(By.Id("retry"));
+                    //retry.Clear();
+                    //retry.SendKeys("2"); 
+                    //IWebElement save = driver.FindElement(By.Id("save"));
+                    //save.Click();
+                    //#region ADD PROXY
+                    driver.Navigate().GoToUrl("chrome-extension://ggmdpepbjljkkkdaklfihhngmmgmpggp/options.html");
+                    driver.SwitchTo().Window(driver.WindowHandles.Last());
+                    driver.Close();
+                    driver.SwitchTo().Window(driver.WindowHandles.First());
+                    driver.FindElement(By.Id("login")).SendKeys(UserName);
+                    driver.FindElement(By.Id("password")).SendKeys(Password);
+                    driver.FindElement(By.Id("retry")).Clear();
+                    driver.FindElement(By.Id("retry")).SendKeys("2");
+                    driver.FindElement(By.Id("save")).Click();
+                    //#endregion
+                }
+            }
+            //IReadOnlyCollection<string> windowHandles = driver.WindowHandles;
+
+            //string firstTab = windowHandles.First();
+
+            //string lastTab = windowHandles.Last();
+            //driver.CurrentWindowHandle.EndsWith(firstTab);
+            //driver.SwitchTo().Window(firstTab);
+           
+            driver.Navigate().GoToUrl("https://mbasic.facebook.com");
+            string taikhoan = Txttaikhoan.Text;
+            string[] Chuoitaikhoan = taikhoan.Split("|");
+            IWebElement Tkfb = driver.FindElement(By.Name("email"));
+            Tkfb.SendKeys(Chuoitaikhoan[0]);
+            IWebElement Mkfb = driver.FindElement(By.Name("pass"));
+            Mkfb.SendKeys(Chuoitaikhoan[1]);
+            try 
+            {
+                IWebElement Login = driver.FindElement(By.Name("login"));
+                Login.Click();
+            }catch (Exception ex)
+            {
+                MessageBox.Show("Tài khoản hoặc mật khẩu không chính xác");
+            }
+
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--headless");
+            //driver = new ChromeDriver(chromeOptions);
+            ChromeDriver driveran = new ChromeDriver(service, chromeOptions);
+            driveran.Navigate().GoToUrl("https://2fa.live/");
+            IWebElement token = driveran.FindElement(By.Id("listToken"));
+            token.SendKeys(Chuoitaikhoan[2]);
+            IWebElement submit = driveran.FindElement(By.Id("submit"));
+            submit.Click();
+            IWebElement Code2fa = driveran.FindElement(By.Id("copy_btn"));
+            //string ma2fa = Code2fa.GetAttribute("placeholder");
+            //Txt2fa.Text = ma2fa;
+            Code2fa.Click() ;
+            Thread.Sleep(3000);
+            Txt2fa.Paste();
+            driveran.Close();
+            driveran.Quit();
+            try
+            {
+                IWebElement Nhap2fa = driver.FindElement(By.Id("approvals_code"));
+                Nhap2fa.Click();
+            }
+            catch
+            {
+                Thread.Sleep(3000);
+                driver.Navigate().Refresh();
+                driver.Navigate().GoToUrl("https://mbasic.facebook.com");
+            }
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void TabConfiguration_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void BtnLoginmail_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string Mails = "Hotmail";
+                ChromeDriverService HideCmd = ChromeDriverService.CreateDefaultService();
+                HideCmd.HideCommandPromptWindow = true;
+                ChromeOptions Options = new ChromeOptions();
+                Options.AddArgument("--window-size=" + 400 + "," + 600);
+                string taikhoan = Txttaikhoan.Text;
+                string[] Chuoitaikhoan = taikhoan.Split("|");
+                string Mail = Chuoitaikhoan[3];
+                string Passmail = Chuoitaikhoan[4];
+                string[] ChuoiTKMail = Mail.Split("@");
+                if (Chuoitaikhoan.Count() > 3)
+                {
+
+
+                    if (ChuoiTKMail[1].Length < 9)
+                    {
+                        MessageBox.Show("Mail của bạn không đúng định dạng Hotmail hoặc Gmail", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else if(ChuoiTKMail[1].Trim().Length == 9)
+                    {
+                        ChromeDriver Openchrome = new ChromeDriver(HideCmd, Options);
+                        Openchrome.Navigate().GoToUrl("https://accounts.google.com/v3/signin/identifier?dsh=S956487633%3A1664956326643983&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&rip=1&sacu=1&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=AQDHYWpoOKdag5s-WgmQefagnCHnKx_FzDEOSW-mCOi6Rs9DbrE8yXF8hrNhnTX7134k-39rx2CaqQ");
+                        IWebElement Idmail = Openchrome.FindElement(By.XPath("/html/body/div[1]/div[1]/div[2]/div/c-wiz/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input"));
+                        Idmail.SendKeys(Mail);
+                        Thread.Sleep(2000);
+                        Idmail.SendKeys(OpenQA.Selenium.Keys.Enter);
+                    }
+                    else
+                    {
+                        ChromeDriver Openchrome = new ChromeDriver(HideCmd, Options);
+                        Openchrome.Navigate().GoToUrl("https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1664961454&rver=7.0.6737.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fnlp%3d1%26RpsCsrfState%3d55237ceb-5959-48fa-ad4a-4ab7a26c9fe5&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=90015");
+                        IWebElement Idmail = Openchrome.FindElement(By.XPath("/html/body/div/form[1]/div/div/div[2]/div[1]/div/div/div/div/div[1]/div[3]/div/div/div/div[2]/div[2]/div/input[1]"));
+                        Idmail.SendKeys(Mail);
+                        Thread.Sleep(2000);
+                        Idmail.SendKeys(OpenQA.Selenium.Keys.Enter);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Đéo có mail");
+                    Txttaikhoan.Focus();
+                }
+
+            }catch (Exception ex)
+
+            {
+                MessageBox.Show(ex.Message ,"Enrror" , MessageBoxButtons.OK , MessageBoxIcon.Error);
+            }
+        }
+    }
+
+
+}
